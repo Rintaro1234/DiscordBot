c823e568971470e453b0e8d165f0a881c5689ffb
Rintaro <rin.nagaya.00@gmail.com>
2022-04-06 22:36:05 +0900

    TOKEN

--- main.py
+++ main.py
@@ -109,8 +109,8 @@ def playSound():
 def generate_wav(text, speaker=2, filepath='./audiosources/audio.wav'):
     text = romkan.to_katakana(text)
     text = text.replace('0', 'ゼロ').replace('1', 'イチ').replace('2', 'ニ').replace('3', 'サン').replace('4', 'ヨン').replace('5', 'ゴー').replace('6', 'ロク').replace('7', 'ナナ').replace('8', 'ハチ').replace('9', 'キュウ')
-    #response2 = requests.post("https://api.su-shiki.com/v2/voicevox/audio/?key=" + os.environ['VoiceToken'] + "&speaker=" + str(speaker) + "&pitch=0&intonationScale=1&speed=1.0&text=" + text)
-    response2 = requests.post("https://api.su-shiki.com/v2/voicevox/audio/?key=U_4463J7F-B9f-5&speaker=" + str(speaker) + "&pitch=0&intonationScale=1&speed=0.9&text=" + text)
+    response2 = requests.post("https://api.su-shiki.com/v2/voicevox/audio/?key=" + os.environ['VoiceToken'] + "&speaker=" + str(speaker) + "&pitch=0&intonationScale=1&speed=1.0&text=" + text)
+    
     wf = wave.open(filepath, 'wb')
     wf.setnchannels(1)
     wf.setsampwidth(2)
@@ -118,5 +118,4 @@ def generate_wav(text, speaker=2, filepath='./audiosources/audio.wav'):
     wf.writeframes(response2.content)
     wf.close()
 
-#bot.run(os.environ['TOKEN'])
-bot.run("OTUwNjYzNDUwNDYyNDAwNTQz.YicMVQ.FPq3d7vswRmKsAj7vvsQv_Izigg")
+bot.run(os.environ['TOKEN'])
